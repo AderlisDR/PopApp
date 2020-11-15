c7a3eb453f327a5f0912dec6181d4f3a20836cf5
Aderlis Diaz Rojas <aderlis1@gmail.com>
2020-11-15 18:27:25 -0400

    Datagrid styling

--- PopAppAPI/ClientApp/src/app/services/document/document.service.ts
+++ PopAppAPI/ClientApp/src/app/services/document/document.service.ts
@@ -11,19 +11,19 @@ export class DocumentService {
   GetDocuments(): Promise<Document[]>{
     return this.http.get<Document[]>(`${this.baseUrl}/document`).toPromise();
   }
-  
+
   GetDocument(id: number): Promise<Document>{
     return this.http.get<Document>(`${this.baseUrl}/document/${id}`).toPromise();
   }
-  
+
   PostDocument(document: Document){
     return this.http.post(`${this.baseUrl}/document` , document).toPromise();
   }
-  
+
   PutDocument(document: Document , id: number){
     return this.http.put(`${this.baseUrl}/document/${id}` , document).toPromise();
   }
-  
+
   DeleteDocument(id: number){
     return this.http.delete(`${this.baseUrl}/document/${id}`).toPromise();
   }
