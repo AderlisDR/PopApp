228877428fb9539cb5d7fa01027d8669e63c5a74
javaChain <jorgedavidterreri@gmail.com>
2020-11-15 20:42:11 -0400

    user-grid implements

---
+++ PopAppAPI/ClientApp/src/app/pages/user-grid/user-grid.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user/user';
+
+@Component({
+  selector: 'app-user-grid',
+  templateUrl: './user-grid.component.html',
+  styleUrls: ['./user-grid.component.scss']
+})
+export class UserGridComponent implements OnInit {
+
+  userData: User[];
+  groupPanelTexts = {
+    groupByThisColumn: 'Agrupar por esta columna',
+    groupContinuedMessage: 'Continuación desde la página anterior',
+    groupContinuesMessage: 'Continúa en la siguiente página'
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onExport(){}
+
+}
