d5c8e01b4a27c41df671b7e8125514fa9ce4a401
javaChain <jorgedavidterreri@gmail.com>
2020-12-01 21:30:53 -0400

    create containerTypeModel services ..

---
+++ PopAppCore/Dtos/ContainerTypeModelDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PopAppCore.Dtos
+{
+    public class ContainerTypeModelDto
+    {
+        public int Id { get; set; }
+        public string ContainerType { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
